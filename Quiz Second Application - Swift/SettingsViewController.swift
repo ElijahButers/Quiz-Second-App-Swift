de1a2c00fa9c8d75d45bb9e1fafb419fb0876bcf
ElijahButers <Elijahbuters@gmail.com>
2015-09-22 01:24:36 +0300

    Basic Navigation: Added: 2 IBActions, 1 Outlet, + code.

--- Quiz Second Application - Swift/SettingsViewController.swift
+++ Quiz Second Application - Swift/SettingsViewController.swift
@@ -10,18 +10,67 @@ import UIKit
 
 class SettingsViewController: UIViewController {
 
+    
+    @IBOutlet var gameModeButtons: [UIButton]!
+    
+    @IBAction func gameModeButtonHadler(sender: UIButton) {
+        
+        for button in gameModeButtons {
+            if sender != button {
+                button.alpha = 0.7
+            } else {
+                button.alpha = 1.0
+                gameMode = button.tag
+            }
+        }
+    }
+    @IBAction func playButtonHandler(sender: UIButton) {
+        
+        var vc: UIViewController?
+        switch gameMode {
+        case 0:
+            vc = storyboard?.instantiateViewControllerWithIdentifier("multipleChoiceViewController") as! MultipleChoiceViewController
+            break
+        case 1:
+            vc = storyboard?.instantiateViewControllerWithIdentifier("rightOrWrongViewController") as! RightOrWrongViewController
+            break
+        case 2:
+            vc = storyboard?.instantiateViewControllerWithIdentifier("inputViewController") as! InputViewController
+            break
+        case 3:
+            vc = storyboard?.instantiateViewControllerWithIdentifier("imageViewController") as! ImageViewController
+            break
+        default:
+            break
+        }
+        
+        if vc != nil {
+        navigationController?.pushViewController(vc!, animated: true)
+        }
+    }
+
+    var gameMode = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
         view.backgroundColor = UIColor(patternImage: UIImage(named: "Night sky-png")!)
+
     }
     
     override func viewWillAppear(animated: Bool) {
         super.viewWillAppear(true)
         
         navigationController?.setNavigationBarHidden(false, animated: false)
+        
+        for (idx, button) in EnumerateSequence(gameModeButtons) {
+            button.tag = idx
+            if button.tag != 0 {
+                button.alpha = 0.7
+            }
+        }
     }
 
 
