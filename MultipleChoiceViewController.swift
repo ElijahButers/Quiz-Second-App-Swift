d6b4bcbee064ddb309945c631f6204b4b1f6c136
ElijahButers <Elijahbuters@gmail.com>
2015-09-22 22:38:40 +0300

    Added: Play Game Button + 3 Views + 1 label + 4 buttons + 1 progress bar + constraints.

--- MultipleChoiceViewController.swift
+++ MultipleChoiceViewController.swift
@@ -14,6 +14,7 @@ class MultipleChoiceViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "Night sky-png")!)
     }
 
     override func didReceiveMemoryWarning() {
